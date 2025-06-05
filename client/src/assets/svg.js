02de4e873a0ae7a58ca49c63ebce3f00f31262e2
Hripsime <m_hripsime@mail.ru>
2025-06-05 15:25:43 +0400

    Added Stories block in Education page

--- client/src/assets/svg.js
+++ client/src/assets/svg.js
@@ -1,2 +1,9 @@
+// Header
 export const burgerIcon = "burger";
 export const crossIcon = "cross";
+
+//Education
+
+export const starStoryIcon = "starStory";
+ 
+
--- client/src/components/EducationPage/EducationStories/EducationStories.jsx
+++ client/src/components/EducationPage/EducationStories/EducationStories.jsx
@@ -1,29 +1,80 @@
 import styles from "./EducationStories.module.scss";
+import ImageWebp from "../../layout/ImageWebp/ImageWebp";
+import Svg from "../../layout/Svg/Svg";
+
+import {
+  storyImage, 
+  storyWebpImage
+} from "../../../assets/images";
+
+import {
+  starStoryIcon 
+} from "../../../assets/svg";
 
 const EducationStories = () => {
   return (
-   <section className={`${styles.educationStories} wrapperWhite`}>
-    <div className="container">
+    <section className={`${styles.educationStories} wrapperWhite`}>
       <h2 className="titleBlackh2">Success stories of the&nbsp;          
         <span className="titlePurpleh2">students placed abroad             
         </span>
       </h2>  
 
       <div className={styles.educationStories__cardContainer}> 
-         <div className={styles.educationStories__card}> 
-          <p>
+        <div className={styles.educationStories__card}> 
+          <p className={styles.educationStories__cardText}>
             As I navigated the challenge of choosing the right destination for my academic journey, my focus was primarily on my passion for football. MAMNUN Agency played a crucial role in helping me identify the best educational path for my future. Applying to study in Warsaw turned out to be one of the best decisions of my life. Not only am I pursuing my diploma, which is a significant step towards my career, but I also have the incredible opportunity to play for one of the top football teams in Poland. Thanks to MAMNUN, I am successfully balancing my education and my love for the game!
-          </p>
+          </p>          
+          <hr className={styles.educationStories__cardLine} />
 
-         </div>
+          <div className={styles.educationStories__storyContainer}> 
+            <div className={styles.educationStories__storyBLock}>
+              <ImageWebp 
+                src={storyImage} 
+                srcSet={storyWebpImage}
+                alt="Success stories of the students"                 
+                className={styles.educationStories__storyImg} 
+              /> 
+
+              <div className={styles.educationStories__storyInfo}>
+                <span className={styles.educationStories__studientName}>
+                  Jony
+                </span>
+                <span className={styles.educationStories__storyTitle}>
+                  Finding My Path to Poland
+                </span>
+              </div>
+            </div>
+
+            <div className={styles.educationStories__starBlock}>
+              <Svg 
+                id={starStoryIcon}
+                className={styles.educationStories__starIcon}
+              />
+                <Svg 
+                id={starStoryIcon}
+                className={styles.educationStories__starIcon}
+              />
+                <Svg 
+                id={starStoryIcon}
+                className={styles.educationStories__starIcon}
+              />
+                <Svg 
+                id={starStoryIcon}
+                className={styles.educationStories__starIcon}
+              />
+                <Svg 
+                id={starStoryIcon}
+                className={styles.educationStories__starIcon}
+              />
+            </div> 
+          </div>        
+
+
+
+        </div>
         
-      </div>
-
-
-      
-       
-    </div>      
-   </section>  
+      </div>   
+    </section>  
   );
 };
 
