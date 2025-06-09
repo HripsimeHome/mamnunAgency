b91e82350e3f0aaf8b55c43ad404eb3ab3cd42d4
Hripsime <m_hripsime@mail.ru>
2025-06-09 17:21:19 +0400

    Added block Our Vision, Our Mission in About page

--- client/src/components/AboutPage/AboutHeader/AboutHeader.jsx
+++ client/src/components/AboutPage/AboutHeader/AboutHeader.jsx
@@ -16,22 +16,53 @@ const AboutHeader = () => {
             <h2 className={styles.aboutHeader__title}>
               MAMNUN is not just an education consultancy agency
             </h2>  
-            <p>
+            <p  className={styles.aboutHeader__firstParagraph}>
               We are also a premier travel agency dedicated to providing unique travel packages and exceptional services. Our expertise extends beyond education, as we offer VIP passenger ground handling with Fast Track assistance at airports, ensuring a seamless travel experience for our clients. 
             </p>
-            <p>
+            <p className={styles.aboutHeader__secondParagraph}>
             Whether you are seeking educational opportunities or planning your next adventure, MAMNUN is here to cater to all your travel needs, including ground transportation across the globe. Experience the perfect blend of education and travel with MAMNUN, where your satisfaction is our top priority!
             </p>
-          </div>
-        
+          </div>        
         
           <ImageWebp 
             src={logoBlackImage} 
             srcSet={logoBlackWebpImage}
             alt="MAMNUN Agency"                 
-            className={styles.aboutHeader__logo} 
+            className={styles.aboutHeader__logoAnim} 
           />
-        </div>      
+        </div>{/* content */}
+
+
+
+
+      <div className={styles.aboutHeader__briefContainer}> 
+        <div className={styles.aboutHeader__briefBlock}> 
+          <h2 className="titleBlackh2">Our&nbsp;
+            <span className="titlePurpleh2">Vision             
+            </span>
+          </h2>  
+          <p className={styles.aboutHeader__description}>               
+            To bridge global learning opportunities, providing diverse educational programs worldwide to students while crafting unique travel experiences and memorable journeys customized to individual aspirations. Through education and travel, we strive to foster a global perspective, and cultural understanding.
+          </p> 
+        </div>
+
+
+         <div className={styles.aboutHeader__briefBlock}> 
+          <h2 className="titleBlackh2">Our&nbsp;
+            <span className="titlePurpleh2">Mission             
+            </span>
+          </h2>  
+          <p className={styles.aboutHeader__description}>               
+            To challenge cultural stereotypes by promoting education and travel as opportunities for personal and professional growth for everyone.
+          </p> 
+        </div>
+
+        </div>
+        {/* briefContainer */}
+
+
+
+
       </div>      
    </section>  
   );
