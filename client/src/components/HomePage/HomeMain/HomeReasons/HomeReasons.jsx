2ea0096bfeebf1fd178f2795cf07b387eca0f6f5
Hripsime <m_hripsime@mail.ru>
2025-06-11 17:20:58 +0400

    Added accordion in Home  page

---
+++ client/src/components/HomePage/HomeMain/HomeReasons/HomeReasons.jsx
@@ -0,0 +1,15 @@
+import styles from "./HomeReasons.module.scss";
+
+const HomeReasons = () => {
+  return (        
+    <section className={styles.homeReasons}> 
+
+      <h2 className="titleWhiteh2">Reasons to&nbsp;
+        <span className="titlePurpleh2">Choouse Us             
+        </span>
+      </h2> 
+    </section>  
+  );
+};
+
+export default HomeReasons;
