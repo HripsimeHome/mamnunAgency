0663262180a754c4dc3685d81416690e254c4882
Hripsime <m_hripsime@mail.ru>
2025-06-12 14:45:15 +0400

    Added flip cards in ServicesTraveller component

--- client/src/components/ServicesPage/ServicesTraveller/ServicesTraveller.jsx
+++ client/src/components/ServicesPage/ServicesTraveller/ServicesTraveller.jsx
@@ -1,4 +1,10 @@
 import styles from "./ServicesTraveller.module.scss";
+import { Link } from "react-router-dom"
+import {
+  contactsPagePath
+} from "../../../router/path"
+
+
 
 const ServicesTraveller = () => {
   return (
@@ -8,18 +14,88 @@ const ServicesTraveller = () => {
           <span className="titlePurpleh2">is Unique         
           </span>
         </h2>  
+        <p className={styles.ervicesTraveller__text}>
+          We customize our services to perfectly align with your individual needs.
+        </p>
  
 
-        <div className={styles.servicesTraveller__flipCard}>
-          <div className={styles.servicesTraveller__flipCardInner}>
-            <div className={styles.servicesTraveller__front}>
-              Front
-            </div>
-            <div className={styles.servicesTraveller__back}>
-              Back
+
+
+        <div className={styles.servicesTraveller__cardContainer}>
+          <div className={styles.servicesTraveller__card}>
+            <div className={styles.servicesTraveller__cardInner}>
+              <div className={styles.servicesTraveller__cardFront}>
+                Front
+              </div>
+
+
+              
+              <div className={styles.servicesTraveller__cardBack}>
+                <h4 className={styles.servicesTraveller__cardBackTitle}>
+                  Corporate Groups and Incentive Travel                  
+                </h4>
+                <siv className={styles.servicesTraveller__cardBackTextBlock}>
+                  <p className={styles.servicesTraveller__cardBackText}>
+                    We specialize in organizing seamless travel arrangements for corporate groups. Our comprehensive services, including group bookings and dedicated coordinators, ensure that your team can focus on what matters most.
+                  </p>
+
+                  <Link
+                    to={contactsPagePath}
+                    className="btnPrimary"
+                  >             
+                  Learn more
+                  </Link>
+                </siv>
+
+
+              </div>
+              {/* Back */}
             </div>
           </div>
+          {/* flipCard */}
+
+
+         <div className={styles.servicesTraveller__card}>
+            <div className={styles.servicesTraveller__cardInner}>
+              <div className={styles.servicesTraveller__cardFront}>
+                Front
+              </div>
+              
+              <div className={styles.servicesTraveller__cardBack}>
+                Back
+                <Link
+                to={contactsPagePath}
+                className="btnPrimary"
+                >             
+                Learn more
+                </Link>
+              </div>
+            </div>
+          </div>
+          {/* flipCard */}
+
+          <div className={styles.servicesTraveller__card}>
+            <div className={styles.servicesTraveller__cardInner}>
+              <div className={styles.servicesTraveller__cardFront}>
+                Front
+              </div>
+              
+              <div className={styles.servicesTraveller__cardBack}>
+                Back
+                <Link
+                to={contactsPagePath}
+                className="btnPrimary"
+                >             
+                Learn more
+                </Link>
+              </div>
+            </div>
+          </div>
+          {/* flipCard */}
         </div>
+        {/* flipCardContainer */}
+
+
 
 
     
--- client/src/components/TourismPage/TourismDestinations/TourismDestinations.jsx
+++ client/src/components/TourismPage/TourismDestinations/TourismDestinations.jsx
@@ -28,6 +28,7 @@ const destinationsData = [
   {
     img: samarkandImage,
     webpImg: samarkandWebpImage,
+    alt: "Samarkand",
     title: "Samarkand",
     description:      
     <>The jewel of the Silk Road, <strong>Samarkand</strong> is famous for its breathtaking architectural wonders. Visit the <strong>Registan Square, Gur-e-Amir Mausoleum</strong>, and the iconic <strong>Shah-i-Zinda complex</strong>. Samarkand is also known for its rich crafts, including <strong>silk paper</strong> production and <strong>carpet weaving</strong>. The city’s <strong>Samarkand bread</strong> is a must-try, and you can witness the ancient art of <strong>calligraphy</strong> here.
@@ -36,17 +37,21 @@ const destinationsData = [
   {
     img: bukharaImage,
     webpImg: bukharaWebpImage,
-    alt: "Samarkand",
+    alt: "Bukhara",
     title: "Bukhara",
-    description: "Step back in time in Bukhara, an ancient city steeped in history. The Bukhara Ark (fortress), Bolo Haouz Mosque, and Ismail Samani Mausoleum are essential landmarks. Bukhara is the birthplace of miniature painting, and its master artisans still create stunning works that capture the city’s essence. Also, don’t miss the famous Nadir Divan-Beghi Khanqah and its mesmerizing tile work."
+    description: 
+    <>Step back in time in <strong>Bukhara</strong>, an ancient city steeped in history. The <strong>Bukhara Ark</strong> (fortress), <strong>Bolo Haouz Mosque</strong>, and <strong>Ismail Samani Mausoleum</strong> are essential landmarks. Bukhara is the birthplace of <strong>miniature painting</strong>, and its master artisans still create stunning works that capture the city’s essence. Also, don’t miss the famous <strong>Nadir Divan-Beghi Khanqah</strong> and its mesmerizing tile work.
+    </>
   },
   
   {
     img: khivaImage,
     webpImg: khivaWebpImage,
-    alt: "Bukhara",
+    alt: "Khiva",
     title: "Khiva",
-    description: "A preserved oasis city, Khiva feels like stepping into a living museum. Wander through the Itchan Kala (the walled inner city) and marvel at the Kalta Minor Minaret and Juma Mosque. Khiva is renowned for its ceramics, and you can visit local workshops where these traditional crafts are still made by hand. Explore the Karakum Desert and Kyzylkum Desert, where you can take part in camel rides or enjoy stargazing by night."
+    description: 
+    <>A preserved oasis city, <strong>Khiva</strong> feels like stepping into a living museum. Wander through the <strong>Itchan Kala</strong> (the walled inner city) and marvel at the <strong>Kalta Minor Minaret</strong> and <strong>Juma Mosque</strong>. Khiva is renowned for its <strong>ceramics</strong>, and you can visit local workshops where these traditional crafts are still made by hand. Explore the <strong>Karakum Desert</strong> and <strong>Kyzylkum Desert</strong>, where you can take part in camel rides or enjoy stargazing by night.
+    </>
   },
   
   {
@@ -54,7 +59,9 @@ const destinationsData = [
     webpImg: tashkentWebpImage,
     alt: "Tashkent",
     title: "Tashkent",
-    description: "The capital city, Tashkent, offers a mix of modernity and ancient history. Visit the Khast Imam Complex, which houses the ancient Uthman Quran, one of the oldest copies of the Quran in the world. The Chorsu Bazaar is an excellent place to experience local life and taste delicious Uzbek street food. Don’t miss a stroll in the city’s Amir Timur Square, and enjoy the blend of Soviet-era architecture with modern buildings."
+    description: 
+    <>The capital city, <strong>Tashkent</strong>, offers a mix of modernity and ancient history. Visit the <strong>Khast Imam Complex</strong>, which houses the ancient <strong>Uthman Quran</strong>, one of the oldest copies of the Quran in the world. The <strong>Chorsu Bazaar</strong> is an excellent place to experience local life and taste delicious Uzbek street food. Don’t miss a stroll in the city’s<strong> Amir Timur Square</strong>, and enjoy the blend of Soviet-era architecture with modern buildings.
+    </>
   },
   
   {
@@ -62,7 +69,9 @@ const destinationsData = [
     webpImg: nukusWebpImage,
     alt: "Nukus & the Savitsky Museum",
     title: "Nukus & the Savitsky Museum",
-    description: "In Nukus, you will find the Savitsky Museum, home to one of the largest collections of Russian avant-garde art outside of Russia. This lesser-known destination offers an off-the-beaten-path experience of Uzbekistan’s rich art and cultural history."
+    description: 
+    <>In <strong>Nukus</strong>, you will find the <strong>Savitsky Museum</strong>, home to one of the largest collections of Russian avant-garde art outside of Russia. This lesser-known destination offers an off-the-beaten-path experience of Uzbekistan’s rich art and cultural history.
+    </>
   },
   
   {
@@ -70,7 +79,10 @@ const destinationsData = [
     webpImg: chimganWebpImage,
     alt: "Chimgan Mountains & Charvak Reservoir",
     title: "Chimgan Mountains & Charvak Reservoir",
-    description: "For nature lovers and adventurers, the Chimgan Mountains offer excellent hiking, trekking, and skiing in the winter months. The nearby Charvak Reservoir is a perfect spot for water activities like boating, fishing, and relaxing by the shore."
+    description: 
+    <>
+    For nature lovers and adventurers, the <strong>Chimgan Mountains</strong> offer excellent hiking, trekking, and skiing in the winter months. The nearby <strong>Charvak Reservoir</strong> is a perfect spot for water activities like boating, fishing, and relaxing by the shore.
+    </>
   }  
 ]
 
