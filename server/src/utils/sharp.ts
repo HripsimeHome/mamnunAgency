e95dc121f425c6e4b7e2019a39a284941ebe1c1d
Garik <garikgevorgyan.rap@mail.ru>
2025-06-14 18:24:22 +0400

    refactor: simplify resizeImage function by removing size parameter

--- server/src/utils/sharp.ts
+++ server/src/utils/sharp.ts
@@ -1,13 +1,8 @@
 import sharp from "sharp";
 
-export const resizeImage = async (
-  file: string,
-  toFile: string,
-  sizes: [number, number] = [800, 500]
-) => {
+export const resizeImage = async (file: string, toFile: string) => {
   try {
     await sharp(file)
-      .resize(...sizes)
       .toFormat("jpeg")
       .jpeg({ quality: 90 })
       .toFile(`${toFile.slice(0, toFile.lastIndexOf("."))}.jpeg`);
