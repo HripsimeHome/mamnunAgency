7447d6ed532e787a1ea4995f249c34748611a87f
Hripsime <m_hripsime@mail.ru>
2025-06-16 16:55:53 +0400

    added  temp tourism gallery

--- client/src/components/HomePage/HomeMain/HomeMain.jsx
+++ client/src/components/HomePage/HomeMain/HomeMain.jsx
@@ -2,16 +2,12 @@ import styles from "./HomeMain.module.scss";
 import HomeReasons from "./HomeReasons/HomeReasons";
 import HomeServices from "./HomeServices/HomeServices";
 
-
 const HomeMain = () => {
   return (        
     <section className={`${styles.homeMain} wrapperBlack`}>   
-     <div className="container">     
-
-      <HomeReasons />
-       <br />
-      <HomeServices />
- 
+     <div className="container">
+      <HomeReasons />      
+      <HomeServices /> 
       </div> 
     </section>  
   );
