be2879fb95de2a03107428e04b231a797364f12b
Garik <garikgevorgyan.rap@mail.ru>
2025-06-26 14:27:52 +0400

    feat: implement lazy loading for video playback in AboutVideo component

--- client/src/components/AboutPage/AboutMain/AboutVideo/AboutVideo.jsx
+++ client/src/components/AboutPage/AboutMain/AboutVideo/AboutVideo.jsx
@@ -1,11 +1,26 @@
 import styles from "./AboutVideo.module.scss";
+import { useLazy } from "../../../../hooks/useLazy";
+import { useRef } from "react";
+import { useEffect } from "react";
 
 const AboutVideo = () => {
+  const { ref, isInView } = useLazy(undefined, undefined, true);
+  const videoRef = useRef(null);
+  useEffect(() => {
+    if (videoRef.current) {
+      if (isInView) {
+        videoRef.current.play();
+      } else {
+        videoRef.current.pause();
+      }
+    }
+  }, [isInView]);
+
   return (
-    <section className={`${styles.aboutVideo}`}>
+    <section ref={ref} className={`${styles.aboutVideo}`}>
       <div className="container">
         <div className={styles.aboutVideo__videoContainer}>
-          <video className={styles.aboutVideo__video} autoPlay muted loop>
+          <video ref={videoRef} className={styles.aboutVideo__video} loop muted>
             <source src="/videos/about/about.mp4" type="video/mp4" />
             Your browser does not support the video tag.
           </video>
