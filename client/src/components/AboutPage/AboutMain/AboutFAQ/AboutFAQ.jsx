c6ffd2e019a63737500ce9a08e9b06a47092c336
Hripsime <m_hripsime@mail.ru>
2025-06-29 17:00:36 +0400

    Corrected padding in About page

--- client/src/components/AboutPage/AboutMain/AboutFAQ/AboutFAQ.jsx
+++ client/src/components/AboutPage/AboutMain/AboutFAQ/AboutFAQ.jsx
@@ -66,7 +66,7 @@ const AboutFAQ = () => {
   return (
     <section className="wrapperBlack">
       <div className="container">
-        <h2 className="titlePrimaryH2">FAQ</h2>
+        <h2 className="titlePrimaryH2 pbXlg">FAQ</h2>
         <>
           <Accordion accordionItems={accordionItems} />
         </>
