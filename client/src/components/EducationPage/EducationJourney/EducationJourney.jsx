10adc2aa00dd7d411f5a9ee8b397619647861f8a
Hripsime <m_hripsime@mail.ru>
2025-06-30 17:31:34 +0400

    Changed <br /> on max-width

--- client/src/components/EducationPage/EducationJourney/EducationJourney.jsx
+++ client/src/components/EducationPage/EducationJourney/EducationJourney.jsx
@@ -23,10 +23,10 @@ const EducationJourney = () => {
       className={`${styles.educationJourney} wrapperWhite wrapperPadding`}
     >
       <div className="container">
-        <h2 className="titleSecondaryH2 center">
-          Unlock Your Global Education
-          <br />
-          Journey&nbsp;
+        <h2
+          className={`${styles.educationJourney__titlePosition} titleSecondaryH2 center`}
+        >
+          Unlock Your Global Education Journey{" "}
           <span className="titlePrimaryH2">with Us!</span>
         </h2>
 
