01379407a58821b73b88d4aaade1e93a8d51da22
Garik <garikgevorgyan.rap@mail.ru>
2025-07-02 09:05:57 +0400

    fix: simplify Svg component by removing TypeScript FC type annotation

--- client/src/components/layout/Svg/Svg.jsx
+++ client/src/components/layout/Svg/Svg.jsx
@@ -1,8 +1,8 @@
-import React, { FC } from "react";
+import React from "react";
 
 import sprite from "../../../assets/svg/sprite.svg";
 
-const Svg: FC<SvgProps> = ({ className, id }) => {
+const Svg = ({ className, id }) => {
   return (
     <svg className={className}>
       <use href={`${sprite}#${id}`} />
