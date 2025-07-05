f6212284e3da2bc87a5a7afa2bb54dec22a0cd90
Garik <garikgevorgyan.rap@mail.ru>
2025-07-05 12:54:51 +0400

    fix: replace home video to opimized version, and delete poster image, using background instaed

--- client/src/assets/images.js
+++ client/src/assets/images.js
@@ -71,7 +71,6 @@ import visaImage from "./images/footer/cards/visa.png";
 import mvisaWebpImage from "./images/footer/cards/visa.webp";
 
 // Home
-import homeVideoPosterImage from "./images/home/homeVideoPoster.jpg";
 import expertiseImage from "./images/home/icons/reasons/expertise.png";
 import expertiseWebpImage from "./images/home/icons/reasons/expertise.webp";
 
@@ -606,7 +605,6 @@ export {
   mvisaWebpImage,
 
   // Home
-  homeVideoPosterImage,
   expertiseImage,
   expertiseWebpImage,
   personalizedApproachImage,
