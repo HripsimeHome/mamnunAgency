7baf5a63e9ec79c7fd6bec6c06fada38f8e84432
Hripsime <m_hripsime@mail.ru>
2025-07-13 21:28:27 +0400

    Added SpcoalCard section

---
+++ client/src/components/HomePage/HomeSocialCard/HomeSocialCard.jsx
@@ -0,0 +1,24 @@
+function HomeSocialCard() {
+  return (
+    <div style={{ textAlign: "center", padding: "20px" }}>
+      <a
+        href="https://www.instagram.com/mamnunagency/"
+        target="_blank"
+        rel="noopener noreferrer"
+        style={{
+          display: "inline-block",
+          textDecoration: "none",
+          color: "#fff",
+          background: "#C13584",
+          padding: "10px 20px",
+          borderRadius: "8px",
+          fontWeight: "bold",
+        }}
+      >
+        Follow us on Instagram
+      </a>
+    </div>
+  );
+}
+
+export default HomeSocialCard;
