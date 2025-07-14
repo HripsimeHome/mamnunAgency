57e3b553d34235bde09380af1cf914e688821010
Hripsime <m_hripsime@mail.ru>
2025-07-14 16:47:46 +0400

    Added shadow to slider i About page, corrected footer title styles

--- client/src/components/HomePage/HomeSocialCard/HomeSocialCard.jsx
+++ client/src/components/HomePage/HomeSocialCard/HomeSocialCard.jsx
@@ -1,22 +1,19 @@
+import styles from "./HomeSocialCard.module.scss";
+
 function HomeSocialCard() {
   return (
-    <div style={{ textAlign: "center", padding: "20px" }}>
-      <a
-        href="https://www.instagram.com/mamnunagency/"
-        target="_blank"
-        rel="noopener noreferrer"
-        style={{
-          display: "inline-block",
-          textDecoration: "none",
-          color: "#fff",
-          background: "#C13584",
-          padding: "10px 20px",
-          borderRadius: "8px",
-          fontWeight: "bold",
-        }}
-      >
-        Follow us on Instagram
-      </a>
+    <div className={styles.homeSocialCard}>
+      <iframe
+        src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fprofile.php%3Fid%3D61572399132304&tabs=timeline&width=380&height=500&small_header=true&adapt_container_width=true&hide_cover=true&show_facepile=true"
+        width="380"
+        height="400"
+        style={{ border: "none", overflow: "hidden" }}
+        scrolling="no"
+        frameBorder="0"
+        allowFullScreen={true}
+        allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"
+        title="Facebook Page Plugin"
+      />
     </div>
   );
 }
